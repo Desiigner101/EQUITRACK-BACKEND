75a4616201259636d6aab00b101f040cc8fe5134
Kervin Gino Sarsonas <Desiigner4074@gmail.com>
2025-09-27 12:11:16 +0800

    git commit -m "feat: setup Spring Boot backend with MySQL configuration"

---
+++ equitrack-backend/src/main/java/io/equitrack/EquitrackBackendApplication.java
@@ -0,0 +1,13 @@
+package io.equitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquitrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquitrackBackendApplication.class, args);
+	}
+
+}
